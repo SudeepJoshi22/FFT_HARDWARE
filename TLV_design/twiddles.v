293af6b8917fdd0652515350b064a6c343a9387d
SudeepJoshi22 <sudeepj881@gmail.com>
2023-11-24 18:54:10 +0530

    tw

--- TLV_design/twiddles.v
+++ TLV_design/twiddles.v
@@ -1,11 +1,11 @@
 `ifdef N
-	`if (N == 4)
+	`if (N == 8)
 		   wire [31:0] twiddles [0:3];
 		   assign twiddles[0] = 32'h0100_0000;
 		   assign twiddles[1] = 32'h00B5_FF4B;
 		   assign twiddles[2] = 32'h0000_FF00;
 		   assign twiddles[3] = 32'hFF4B_FF4B;
-	`else if(N == 8)
+	`else if(N == 16)
 		   wire [31:0] twiddles [0:7];
 		   assign twiddles[0] = 32'h0100_0000;
 		   assign twiddles[1] = 32'h00B5_FF4B;
